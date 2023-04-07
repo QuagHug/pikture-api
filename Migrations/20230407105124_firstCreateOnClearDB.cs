e242aba9f33a0a5869984157de767acf0d68f23e
Hung Luu <hungluu111103@gmail.com>
2023-04-07 17:54:04 +0700

    fix connection string

---
+++ Migrations/20230407105124_firstCreateOnClearDB.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace piktureAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstCreateOnClearDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    userEmail = table.Column<string>(type: "longtext", nullable: false),
+                    userName = table.Column<string>(type: "longtext", nullable: false),
+                    userAvatarUrl = table.Column<string>(type: "longtext", nullable: false),
+                    imageUrl = table.Column<string>(type: "longtext", nullable: false),
+                    numOfLikes = table.Column<int>(type: "int", nullable: false),
+                    title = table.Column<string>(type: "longtext", nullable: false),
+                    category = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    firstName = table.Column<string>(type: "longtext", nullable: false),
+                    lastName = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
